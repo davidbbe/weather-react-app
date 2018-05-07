907007c84e0e899a131f68109519a2123ce76198
David Beauchamp <david123beauchamp@gmail.com>
2018-05-07 13:58:00 -0600

    Setting up components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import Titles from './components/Titles';
+import Form from './components/Form';
+import Weather from './components/Weather';
+
+const API_KEY = '58ed1117bdbb2b702916632f9fd22117';
 
 class App extends React.Component {
+  getWeather = async () => {
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=oakland,us&appid=${API_KEY}`);
+    const data = await api_call.json();
+    console.log(data);
+  }
+
   render() {
     return (
       <div className="App">
-        <h1>Hello World</h1>
+        <Titles />
+        <Form getWeather={this.getWeather} />
+        <Weather />
       </div>
     );
   }
