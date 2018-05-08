a089812da1bd7aa3c701bb239a448fa6aaa397d1
David Beauchamp <david123beauchamp@gmail.com>
2018-05-07 20:07:34 -0600

    All styled up and lookin nice

--- src/App.js
+++ src/App.js
@@ -18,9 +18,10 @@ class App extends React.Component {
     e.preventDefault();
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`);
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&units=imperial&appid=${API_KEY}`);
     const data = await api_call.json();
-    if (city && country && (data.cod !== '404')) {
+    const apiCallbackCode = data.cod;
+    if (city && country && (apiCallbackCode !== '404')) {
       console.log(data);
       this.setState({
         temperature: data.main.temp,
@@ -30,42 +31,46 @@ class App extends React.Component {
         description: data.weather[0].description,
         error: ''
       });
-    } else {
+    } else if (apiCallbackCode === '404') {
       const apiErrorMessage = data.message;
-      const errorMessage = apiErrorMessage ? apiErrorMessage : "Error submitting data, try again another time";
       this.setState({
         temperature: undefined,
         city: undefined,
         country: undefined,
         humidity: undefined,
         description: undefined,
-        error: errorMessage
+        error: apiErrorMessage
+      });
+    } else {
+      this.setState({
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: 'Error submitting data, try again another time'
       });
     }
   }
 
   render() {
     return (
-      <div>
-        <div className="wrapper">
-          <div className="main">
-            <div className="container">
-              <div className="row">
-                <div className="col-sm title-container">
-                  <Titles />
-                </div>
-                <div className="col-sm form-container">
-                  <Form getWeather={this.getWeather} />
-                  <Weather 
-                    temperature={this.state.temperature}
-                    city={this.state.city}
-                    country={this.state.country}
-                    humidity={this.state.humidity}
-                    description={this.state.description}
-                    error={this.state.error}
-                  />
-                </div>
-              </div>
+      <div className="wrapper">
+        <div className="container">
+          <div className="row">
+            <div className="col-sm title-container">
+              <Titles />
+            </div>
+            <div className="col-sm form-container">
+              <Form getWeather={this.getWeather} />
+              <Weather 
+                temperature={this.state.temperature}
+                city={this.state.city}
+                country={this.state.country}
+                humidity={this.state.humidity}
+                description={this.state.description}
+                error={this.state.error}
+              />
             </div>
           </div>
         </div>
--- src/components/Titles.js
+++ src/components/Titles.js
@@ -4,7 +4,7 @@ const Titles = () => {
   return (
     <div>
       <h1 className="title-container__title">Weather Finder</h1>
-      <h3 className="title-container__subtitle">Find out temperature, conditions and more...</h3>
+      <h3 className="title-container__subtitle">Input any city and country to get weather information.</h3>
     </div>
   );
 }
